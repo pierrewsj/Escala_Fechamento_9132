bf4293235542561e41fd6a3ad9de1d18dc7162f8
pierrewsj <pierrewenderson1@gmail.com>
2026-07-27 11:40:59 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.APP_CONFIG = Object.freeze({
+  versao: '2.2.0',
+  equipe: '9132',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzK3qWWsEa1n3mkS1ADmVcw2LBlrJ3ZyGjFzNTUQpePtnxAM8WNgaxj1e5_a9cOvtWZgg/exec',
+  adminHashPartes: [
+    'fe2592b42a727e977f055947385b709c',
+    'c82b16b9a87f88c6abf3900d65d0cdc3'
+  ],
+  timeoutApiMs: 12000
+});
